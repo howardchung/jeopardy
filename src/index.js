0c41f331327c6a01f28f8ccff9e0c877bd1c58f2
Ubuntu <howardzchung@gmail.com>
2023-02-14 10:35:43 +0000

    update all the other deps

--- src/index.js
+++ src/index.js
@@ -1,18 +1,16 @@
 import './index.css';
 import 'semantic-ui-css/semantic.min.css';
 
-import React from 'react';
-import ReactDOM from 'react-dom';
+import {createRoot} from 'react-dom';
 
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 import { JeopardyHome } from './components/Home/Home';
 
 const isHome = !Boolean(window.location.hash.substring(1));
-ReactDOM.render(
-  <React.StrictMode>{isHome ? <JeopardyHome /> : <App />}</React.StrictMode>,
-  document.getElementById('root')
-);
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(isHome ? <JeopardyHome /> : <App />);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
