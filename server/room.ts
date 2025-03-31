5b629795fd303c19f8b3cfbcf269498e24a6dd1c
Howard Chung <howardzchung@gmail.com>
2025-03-31 13:42:49 -0700

    Update room.ts
    Former-commit-id: 4e0461b4e72aee5fa0eccc43a6588ea7856017e6 [formerly 0341b7510a820d207aa40692a94402cb40c157a6]
    Former-commit-id: 42cc072e5a339c4208939452b6d05e82ee65cd80

--- server/room.ts
+++ server/room.ts
@@ -211,12 +211,16 @@ export class Room {
       socket.on('JPD:wager', (wager) => this.submitWager(socket.id, wager));
       socket.on('JPD:judge', (data) => this.doHumanJudge(socket, data));
       socket.on('JPD:bulkJudge', (data) => {
+        if (!data) {
+          return;
+        }
         // Check if the next player to be judged is in the input data
         // If so, doJudge for that player
         // Check if we advanced to the next question, otherwise keep doing 
         let count = 0;
         while (this.jpd.public.currentJudgeAnswer !== undefined && count <= data.length) {
           // The bulkjudge may not contain all decisions. Stop if we did as many decisions as the input data
+          count += 1;
           console.log('[BULKJUDGE]', count, data.length);
           const id = this.jpd.public.currentJudgeAnswer;
           const match = data.find((d: any) => d.id === id);
