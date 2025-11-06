134d9973c327d2f0eb0ac7cc9b7996dfc92473bc
Howard Chung <howardzchung@gmail.com>
2025-11-06 07:04:47 +0000

    try out hono
    Former-commit-id: a1ec17e3ff0bb8cdc5d01d7d1815a29fbb4d6270 [formerly ccd78f5712b3710bd819e46a210df684b21782c8]
    Former-commit-id: 55cb34af323a6290d0519e8853be2ee15ffe119b

--- server/server.ts
+++ server/server.ts
@@ -1,11 +1,10 @@
-import fs from 'fs';
-import util from 'util';
-import express from 'express';
-import compression from 'compression';
-import os from 'os';
-import cors from 'cors';
-import https from 'https';
-import http from 'http';
+import util from 'node:util';
+import { Hono } from 'hono';
+import { compress } from 'hono/compress';
+import { serve } from '@hono/node-server';
+import { serveStatic } from '@hono/node-server/serve-static';
+import { cors } from 'hono/cors';
+import os from 'node:os';
 import { Server } from 'socket.io';
 import { Room } from './room.ts';
 import { redis, getRedisCountDay } from './redis.ts';
@@ -13,76 +12,21 @@ import { makeRoomName, makeUserName } from './moniker.ts';
 import config from './config.ts';
 import { getJDataStats } from './jData.ts';
 
-const app = express();
-let server = null as https.Server | http.Server | null;
-if (config.SSL_KEY_FILE && config.SSL_CRT_FILE) {
-  const key = fs.readFileSync(config.SSL_KEY_FILE as string);
-  const cert = fs.readFileSync(config.SSL_CRT_FILE as string);
-  server = https.createServer({ key: key, cert: cert }, app);
-} else {
-  server = new http.Server(app);
-}
-const io = new Server(server, {
-  cors: { origin: '*' },
-  transports: ['websocket'],
-});
 const rooms = new Map<string, Room>();
-init();
-setInterval(freeUnusedRooms, 5 * 60 * 1000);
-async function freeUnusedRooms() {
-  // Only run if redis persistence is turned on
-  // Clean up rooms that are no longer in redis and empty
-  // Frees up some JS memory space when process is long-running
-  // If running without redis, keep rooms in memory
-  // We don't currently attempt to reload rooms from redis on demand
-  if (redis) {
-    rooms.forEach(async (room, key) => {
-      if (room.roster.length === 0 && !(await redis?.get(key))) {
-        clearInterval(room.cleanupInterval);
-        rooms.delete(key);
-        // Unregister the namespace to avoid dupes
-        io._nsps.delete(key);
-      }
-    });
-  }
-}
-
-async function init() {
-  if (redis) {
-    // Load rooms from Redis
-    console.log('loading rooms from redis');
-    const keys = await redis.keys('/*');
-    console.log(util.format('found %s rooms in redis', keys.length));
-    for (let i = 0; i < keys.length; i++) {
-      const key = keys[i];
-      const roomData = await redis.get(key);
-      console.log(key, roomData?.length);
-      rooms.set(key, new Room(io, key, roomData));
-    }
-  }
-  config.permaRooms.forEach((roomId) => {
-    // Create the room if it doesn't exist
-    if (!rooms.has(roomId)) {
-      rooms.set(roomId, new Room(io, roomId));
-    }
-  });
-  server?.listen(Number(config.PORT));
-}
-
-app.use(cors());
-app.use(compression());
-app.use(express.static('build'));
-
-app.get('/ping', (req, res) => {
-  res.json('pong');
+const app = new Hono();
+app.use('*', cors());
+app.use('*', compress());
+app.get('/build/*', serveStatic({root: './build' }));
+app.get('/ping', async (req) => {
+  return req.json('pong');
 });
 
-app.get('/metadata', (req, res) => {
-  res.json(getJDataStats());
+app.get('/metadata', async (req) => {
+  return req.json(getJDataStats());
 });
 
-app.get('/stats', async (req, res) => {
-  if (req.query.key && req.query.key === config.STATS_KEY) {
+app.get('/stats', async (req) => {
+  if (req.req.param('key') && req.req.param('key') === config.STATS_KEY) {
     const roomData: any[] = [];
     let currentUsers = 0;
     rooms.forEach((room) => {
@@ -117,7 +61,7 @@ app.get('/stats', async (req, res) => {
     const jeopardyResults = await redis?.llen('jpd:results');
     const aiJudges = await redis?.llen('jpd:aiJudges');
 
-    res.json({
+    return req.json({
       uptime: process.uptime(),
       roomCount: rooms.size,
       cpuUsage,
@@ -141,38 +85,42 @@ app.get('/stats', async (req, res) => {
       rooms: roomData,
     });
   } else {
-    res.status(403).json({ error: 'Access Denied' });
+    req.status(403);
+    return req.json({ error: 'Access Denied' });
   }
 });
 
-app.get('/jeopardyResults', async (req, res) => {
-  if (req.query.key && req.query.key === config.STATS_KEY) {
+app.get('/jeopardyResults', async (req) => {
+  if (req.req.param('key') && req.req.param('key') === config.STATS_KEY) {
     const data = await redis?.lrange('jpd:results', 0, -1);
-    res.json(data);
+    return req.json(data);
   } else {
-    res.status(403).json({ error: 'Access Denied' });
+    req.status(403);
+    return req.json({ error: 'Access Denied' });
   }
 });
 
-app.get('/nonTrivialJudges', async (req, res) => {
-  if (req.query.key && req.query.key === config.STATS_KEY) {
+app.get('/nonTrivialJudges', async (req) => {
+  if (req.req.param('key') && req.req.param('key') === config.STATS_KEY) {
     const data = await redis?.lrange('jpd:nonTrivialJudges', 0, -1);
-    res.json(data);
+    return req.json(data);
   } else {
-    res.status(403).json({ error: 'Access Denied' });
+    req.status(403);
+    return req.json({ error: 'Access Denied' });
   }
 });
 
-app.get('/aiJudges', async (req, res) => {
-  if (req.query.key && req.query.key === config.STATS_KEY) {
+app.get('/aiJudges', async (req) => {
+  if (req.req.param('key') && req.req.param('key') === config.STATS_KEY) {
     const data = await redis?.lrange('jpd:aiJudges', 0, -1);
-    res.json(data);
+    return req.json(data);
   } else {
-    res.status(403).json({ error: 'Access Denied' });
+    req.status(403);
+    return req.json({ error: 'Access Denied' });
   }
 });
 
-app.post('/createRoom', async (req, res) => {
+app.post('/createRoom', async (req) => {
   const genName = () => '/' + makeRoomName();
   let name = genName();
   // Keep retrying until no collision
@@ -183,9 +131,58 @@ app.post('/createRoom', async (req, res) => {
   const newRoom = new Room(io, name);
   newRoom.saveRoom();
   rooms.set(name, newRoom);
-  res.json({ name: name.slice(1) });
+  return req.json({ name: name.slice(1) });
 });
 
-app.get('/generateName', (req, res) => {
-  res.send(makeUserName());
+app.get('/generateName', async (req) => {
+  return req.text(makeUserName());
 });
+
+const server = serve({
+  fetch: app.fetch,
+  // port: Number(config.PORT),
+});
+server.close();
+const io = new Server(server, {
+  cors: { origin: '*' },
+  transports: ['websocket'],
+});
+
+setInterval(freeUnusedRooms, 5 * 60 * 1000);
+async function freeUnusedRooms() {
+  // Only run if redis persistence is turned on
+  // Clean up rooms that are no longer in redis and empty
+  // Frees up some JS memory space when process is long-running
+  // If running without redis, keep rooms in memory
+  // We don't currently attempt to reload rooms from redis on demand
+  if (redis) {
+    rooms.forEach(async (room, key) => {
+      if (room.roster.length === 0 && !(await redis?.get(key))) {
+        clearInterval(room.cleanupInterval);
+        rooms.delete(key);
+        // Unregister the namespace to avoid dupes
+        io._nsps.delete(key);
+      }
+    });
+  }
+}
+
+if (redis) {
+  // Load rooms from Redis
+  console.log('loading rooms from redis');
+  const keys = await redis.keys('/*');
+  console.log(util.format('found %s rooms in redis', keys.length));
+  for (let i = 0; i < keys.length; i++) {
+    const key = keys[i];
+    const roomData = await redis.get(key);
+    console.log(key, roomData?.length);
+    rooms.set(key, new Room(io, key, roomData));
+  }
+}
+config.permaRooms.forEach((roomId) => {
+  // Create the room if it doesn't exist
+  if (!rooms.has(roomId)) {
+    rooms.set(roomId, new Room(io, roomId));
+  }
+});
+server.listen(Number(config.PORT));
